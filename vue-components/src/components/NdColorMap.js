77b29b9e55f8c6789fae1a6494df39aa6c894e77
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2024-04-17 09:16:07 -0600

    fix(d3): use vue to render instead of d3

--- vue-components/src/components/NdColorMap.js
+++ vue-components/src/components/NdColorMap.js
@@ -1,4 +1,4 @@
-import { ref, unref, onMounted, computed, watch } from "Vue";
+import { ref, unref, toRefs, computed } from "Vue";
 import * as d3 from "d3";
 
 import { computeColorMapImage } from "../utils/colors";
@@ -32,6 +32,7 @@ export default {
     },
   },
   setup(props, { emit }) {
+    emit("created");
     const container = ref(null);
     const radRotationAngle = computed(() => props.rotation / 57.32);
     const dataToProcess = computed(() => getData(props.sampleSize));
@@ -44,89 +45,64 @@ export default {
     const bgImage = computed(() =>
       computeColorMapImage(props.size, props.brushMode)
     );
+    const diameter = computed(() => Math.round(props.size * 2.4) / 3.1);
+    const xyOffset = computed(() => (props.size - unref(diameter)) * 0.5);
+    const scaleGBC = computed(() =>
+      d3.scaleLinear(
+        [-1, 1],
+        [unref(xyOffset), unref(xyOffset) + unref(diameter)]
+      )
+    );
 
-    function render() {
-      if (!container.value) {
-        return;
-      }
-      emit("render");
-
-      const headers = unref(dataToProcess).header;
-      const diameter = Math.round(props.size * 2.4) / 3.1;
-      const xyOffset = (props.size - diameter) * 0.5;
-      const scaleGBC = d3.scaleLinear([-1, 1], [xyOffset, xyOffset + diameter]);
-
-      // Root SVG
-      const svgColorSpace = d3
-        .select(container.value)
-        .append("svg")
-        .attr("width", props.size)
-        .attr("height", props.size);
-
-      // Background colorMap
-      svgColorSpace
-        .append("image")
-        .attr("xlink:href", unref(bgImage))
-        .attr("x", 0)
-        .attr("y", 0)
-        .attr("width", props.size)
-        .attr("height", props.size);
-
-      // Scatter points
-      const scatterPoints = svgColorSpace.append("g");
-      scatterPoints
-        .append("svg:g")
-        .selectAll("scatter-dots")
-        .data(dataToDraw.value.q)
-        .enter()
-        .append("circle")
-        .attr("cx", (d) => scaleGBC(d[0]))
-        .attr("cy", (d) => scaleGBC(d[1]))
-        .attr("r", "2.5")
-        .style("fill", "#fff")
-        .style("stroke", "black")
-        .style("stroke-opacity", 0.5);
-
-      // Components points
-      const componentPoints = svgColorSpace.append("g");
-      componentPoints
-        .append("svg:g")
-        .selectAll("scatter-dots")
-        .data(unref(dataToDraw).components)
-        .enter()
-        .append("circle")
-        .attr("cx", (d) => scaleGBC(d[0] * 0.997))
-        .attr("cy", (d) => scaleGBC(d[1] * 0.997))
-        .attr("r", "6")
-        .style("fill", "#C7D9E8")
-        .style("stroke", "#333");
-
-      const labelComponentPoints = svgColorSpace.append("g");
-      labelComponentPoints
-        .append("svg:g")
-        .selectAll("g")
-        .data(unref(dataToDraw).components)
-        .enter()
-        .append("text")
-        .attr("x", (d) => scaleGBC(d[0] * 0.997))
-        .attr("y", (d) => scaleGBC(d[1] * 0.997))
-        .attr("dx", (d) => (d[0] < 0 ? "-1.40em" : ".35em"))
-        .attr("dy", (d) => (d[1] < 0 ? "-.230em" : ".531em"))
-        .text((d, i) => headers[i])
-        .style("font-size", "30px")
-        .style("fill", "#666")
-        .style("stroke", "#111")
-        .style("stroke-opacity", 1)
-        .style("opacity", 1);
-    }
-
-    onMounted(render);
-    watch(() => [props.size, bgImage.value, dataToDraw.value], render);
-
+    const { size } = toRefs(props);
     return {
       container,
+      size,
+      bgImage,
+      dataToDraw,
+      scaleGBC,
+      dataToProcess,
     };
   },
   template: `
-        <div ref="container"></div>`,
+        <div ref="container">
+          <svg :width="size" :height="size">
+            <image :href="bgImage" x="0" y="0" :width="size" :height="size" />
+
+            <g fill="#fff" stroke="black" stroke-opacity="0.5">
+              <circle 
+                :key="i" 
+                v-for="d, i in dataToDraw.q"
+                :cx="scaleGBC(d[0])"
+                :cy="scaleGBC(d[1])"
+                r="2.5"
+              />
+            </g>
+
+            <g fill="#C7D9E8" stroke="#333">
+              <circle 
+                :key="i" 
+                v-for="d, i in dataToDraw.components"
+                :cx="scaleGBC(d[0] * 0.997)"
+                :cy="scaleGBC(d[1] * 0.997)"
+                r="6"
+              />
+            </g>
+
+            <g font-size="30px" fill="#666" stroke="#111" stroke-opacity="1" opacity="1" style="user-select: none;">
+              <text 
+                :key="i" 
+                v-for="d, i in dataToDraw.components"
+                :x="scaleGBC(d[0] * 0.997)"
+                :y="scaleGBC(d[1] * 0.997)"
+                :dx="d[0] < 0 ? '-1.40em' : '.35em'"
+                :dy="d[1] < 0 ? '-.230em' : '.531em'"
+              >
+                {{ dataToProcess.header[i] }}
+              </text>
+            </g>
+
+          </svg>
+        </div>
+  `,
 };
